da429383826e3743d379bb3cb59491db375a2907
dalilia <dalila200875yahoo.fr>
2023-08-08 23:53:34 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+
+
+function superbowlWin(record){
+    
+    const winObject = record.find(obj => obj.result === "W");
+    return winObject ? winObject.year : undefined;
+}
+
+const winningYear = superbowlWin(record);
+console.log("Winning Year:", winningYear); 
